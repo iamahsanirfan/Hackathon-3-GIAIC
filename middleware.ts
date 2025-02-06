67b0a7cc592873f1ad3979957659e3ee80f5095b
Ahsan Paracha <ahsanirfanparacha@gmail.com>
2025-02-07 03:14:06 +0500

    error fixed

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,12 @@
-// src/middleware.ts
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Define the public routes
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/shop',
-  '/search',
-  '/blog',
-  '/contact',
-  '/rocket-seater',
-  '/api/(.*)',
-]);
+export default clerkMiddleware();
 
-export default clerkMiddleware(async (auth, req) => {
-  // Check the pathname of the request
-  
-  }
-  // If it's a public route, simply allow the request to continue.
-);
-
-// Configure Next.js middleware matcher
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
