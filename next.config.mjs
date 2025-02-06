20f91cd14a50f1447604f8ccfe45662357653041
Ahsan Paracha <ahsanirfanparacha@gmail.com>
2025-02-07 02:33:26 +0500

    error

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,16 @@
 /** @type {import('next').NextConfig} */
-// next.config.mjs
 export default {
   images: {
     domains: ['cdn.sanity.io'],
   },
-};
-
-module.exports = {
   env: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-
-    webpack: (config) => {
-      config.resolve.fallback = { 
-        ...config.resolve.fallback,
-        punycode: false 
-      }
-      return config}
   },
-}
-
-
-  
-  
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      punycode: false 
+    };
+    return config;
+  }
+};
